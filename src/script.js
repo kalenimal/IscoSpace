604ca2a6bf0635f7afe9009d46397881498f033e
kaleno <kirillkalinckin@yandex.ru>
2024-06-22 21:30:59 +0700

    vite build

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-import * as Three from "../node_modules/three/build/three.module.js"
-import { FontLoader, OrbitControls, RGBELoader, TextGeometry } from "../node_modules/three/examples/jsm/Addons.js"
+import * as Three from "three"
+import { FontLoader, OrbitControls, RGBELoader, TextGeometry } from "three/examples/jsm/Addons.js"
 import color from "../static/textures/door/color.jpg"
 import metal from "../static/textures/matcaps/3.png"
 import GUI from "lil-gui"
